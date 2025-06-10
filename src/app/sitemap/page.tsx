59d1ccf0cb4f2b18381cada7ec806d71b637b43d
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2025-06-10 23:02:33 +0530

    site map

---
+++ src/app/sitemap/page.tsx
@@ -0,0 +1,194 @@
+// import JoinTeam from "@components/joinTeam";
+import Link from "next/link";
+import styles from "./style.module.scss";
+export default function SiteMap() {
+  return (
+    <main>
+      <section className={styles.siteMapSection}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <ul>
+                <li>
+                  <Link
+                    href="#"
+                    title="#1 Top Ranked Digital Marketig Training Instiute in Asia"
+                  >
+                    #1 Top Ranked Digital Marketig Training Instiute in Asia
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/digital-marketing-courses.php"
+                    title="Digital Marketing Course Online with Placement &amp; Certification"
+                  >
+                    Digital Marketing Course Online with Placement &amp;
+                    Certification
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/master-digital-marketing-leadership-course.php"
+                    title="Digital Marketing &amp; AI Leadership Course | MBA Level Program"
+                  >
+                    Digital Marketing &amp; AI Leadership Course | MBA Level
+                    Program
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/online-digital-marketing-courses.php"
+                    title="Digital Marketing PG Program | Ranked #1 Academy &amp; Class"
+                  >
+                    Digital Marketing PG Program | Ranked #1 Academy &amp; Class
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/digital-marketing-training-institute.php"
+                    title="Best Digital Marketing Training Institute Online &amp; Classroom "
+                  >
+                    Best Digital Marketing Training Institute Online &amp;
+                    Classroom{" "}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/digital-marketing-courses-bangalore.php"
+                    title="Digital Marketing Courses in Bangalore Online &amp; Classroom "
+                  >
+                    Digital Marketing Courses in Bangalore Online &amp;
+                    Classroom{" "}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/online-digital-marketing-courses-bangalore.php"
+                    title="Bangalore’s Awarded Academy for PG In Digital Marketing"
+                  >
+                    Bangalore’s Awarded Academy for PG In Digital Marketing
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/digital-marketing-training-institute-bangalore.php"
+                    title="Online Digital Marketing Training Institute in Bangalore"
+                  >
+                    Online Digital Marketing Training Institute in Bangalore
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/digital-marketing-courses-jayanagar.php"
+                    title="Digital Marketing Courses in Jayanagar With Placement "
+                  >
+                    Digital Marketing Courses in Jayanagar With Placement{" "}
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/online-digital-marketing-courses-jayanagar.php"
+                    title="#1 Academy for Digital Marketing PG Program in Jayanagar"
+                  >
+                    #1 Academy for Digital Marketing PG Program in Jayanagar
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/digital-marketing-training-institute-jayanagar.php"
+                    title="Best Digital Marketing Training Institute in Jayanagar"
+                  >
+                    Best Digital Marketing Training Institute in Jayanagar
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/Linkbout-us.php"
+                    title="Digital Academy 360 | Industry Leaders Since 2015"
+                  >
+                    Digital Academy 360 | Industry Leaders Since 2015
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/placement-report.php"
+                    title="Digital Marketing Course Placements Report &amp; Career Path"
+                  >
+                    Digital Marketing Course Placements Report &amp; Career Path
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/hire-from-da360.php"
+                    title="Hire Digital Marketing Freshers &amp; Interns In Bangalore"
+                  >
+                    Hire Digital Marketing Freshers &amp; Interns In Bangalore
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/refer-and-earn.php"
+                    title="Become Learning Advisor &amp; Earn By Referring Student"
+                  >
+                    Become Learning Advisor &amp; Earn By Referring Student
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/career.php"
+                    title="Join India's Largest Digital Marketing Training Company"
+                  >
+                    Join India&apos;s Largest Digital Marketing Training Company
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/contact-us.php"
+                    title="Help &amp; Support Details Of Digital Academy 360 | Contact Us"
+                  >
+                    Help &amp; Support Details Of Digital Academy 360 | Contact
+                    Us
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/testimonial.php"
+                    title="Digital Academy 360 | Reviews &amp; Testimonials"
+                  >
+                    Digital Academy 360 | Reviews &amp; Testimonials
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/privacy-policy.php"
+                    title="Digital Academy 360 | Privacy Policy"
+                  >
+                    Digital Academy 360 | Privacy Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/terms-conditions.php"
+                    title="Digital Academy 360 Terms &amp; Conditions"
+                  >
+                    Digital Academy 360 Terms &amp; Conditions
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="https://digitalacademy360.com/payment-policy.php"
+                    title="Digital Academy 360 Payment Policy"
+                  >
+                    Digital Academy 360 Payment Policy
+                  </Link>
+                </li>
+
+                <li></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
