8e9e66526dd9f746e2008d0d939de8f3fbc646bd
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2025-06-18 16:33:06 +0530

    photo update

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -8,7 +8,7 @@ const images = [
     size: "small",
   },
   {
-    src: "/images/aboutus-gallery/7-min.jpg",
+    src: "/images/aboutus-gallery/2-min.jpg",
     alt: "Gallery Image 2",
     size: "small",
   },
@@ -23,7 +23,7 @@ const images = [
     size: "small",
   },
   {
-    src: "/images/aboutus-gallery/5-min.jpg",
+    src: "/images/aboutus-gallery/7-min.jpg",
     alt: "Gallery Image 5",
     size: "small",
   },
