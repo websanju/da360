6d7ad4e9c52491be7dfc0a7483e2bcea08256f44
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2025-07-10 16:33:43 +0530

    metatag update for refer-and-earn

--- src/app/refer-and-earn/page.tsx
+++ src/app/refer-and-earn/page.tsx
@@ -4,6 +4,33 @@ import CallToActionBanner from "@components/callToActionBanner";
 import LearningPrograms from "@components/learningPrograms";
 import Faq from "@components/faq";
 
+export const metadata = {
+  title: "Become Learning Advisor & Earn By Referring Student",
+  description:
+    "Refer & earn cash rewards with Digital Academy 360. Help others achieve their education goals by you becoming a learning ambassador",
+  keywords: ["refer & earn, education referral program, refer and earn"],
+  robots:
+    "index, follow, max-snippet:-1, max-video-preview:-1, max-image-preview:large",
+  alternates: {
+    canonical: "https://digitalacademy360.com/refer-and-earn",
+  },
+  openGraph: {
+    title: "Become Learning Advisor & Earn By Referring Student",
+    description:
+      "Refer & earn cash rewards with Digital Academy 360. Help others achieve their education goals by you becoming a learning ambassador",
+    url: "https://digitalacademy360.com/refer-and-earn",
+    siteName: "Digital Academy 360",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Become Learning Advisor & Earn By Referring Student",
+    description:
+      "Refer & earn cash rewards with Digital Academy 360. Help others achieve their education goals by you becoming a learning ambassador",
+  },
+};
+
 export default function ReferEarn() {
   return (
     <main>
