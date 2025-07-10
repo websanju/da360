65164c4d779ab2c89f1dbaa337019b7016b17a90
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2025-07-10 17:14:37 +0530

    metatag update for hire-from-da360

--- src/app/hire-from-da360/page.tsx
+++ src/app/hire-from-da360/page.tsx
@@ -9,6 +9,35 @@ import HireForm from "@components/hireForm";
 import Faq from "@components/faq";
 // import styles from "./style.module.scss";
 
+export const metadata = {
+  title: "Hire Digital Marketing Freshers & Interns In Bangalore",
+  description:
+    "Hire Full Time or Freelance Digital Marketing Freshers & Interns For Free. Get Fresh Digital Marketers With Hands On Project Knowledge & Real Time Experience",
+  keywords: [
+    "Hire Digital Marketing Freshers in Bangalore, Hire Digital Marketing Interns in Bangalore, Hire Digital Marketers in Bangalore, Hire Freelancer For Digital Marketer In Bangalore",
+  ],
+  robots:
+    "index, follow, max-snippet:-1, max-video-preview:-1, max-image-preview:large",
+  alternates: {
+    canonical: "https://digitalacademy360.com/contact-us",
+  },
+  openGraph: {
+    title: "Hire Digital Marketing Freshers & Interns In Bangalore",
+    description:
+      "Hire Full Time or Freelance Digital Marketing Freshers & Interns For Free. Get Fresh Digital Marketers With Hands On Project Knowledge & Real Time Experience",
+    url: "https://digitalacademy360.com/contact-uss",
+    siteName: "Digital Academy 360",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Hire Digital Marketing Freshers & Interns In Bangalore",
+    description:
+      "Hire Full Time or Freelance Digital Marketing Freshers & Interns For Free. Get Fresh Digital Marketers With Hands On Project Knowledge & Real Time Experience",
+  },
+};
+
 export default function HireFrom() {
   return (
     <main>
