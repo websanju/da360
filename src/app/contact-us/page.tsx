4cd289865d143f84e87ecffa502d73e235008c3c
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2025-07-10 16:57:31 +0530

    metatag update for contact-us

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -6,6 +6,35 @@ import LocationCard from "@/components/locationCard";
 import TrustedByLearners from "@/components/trustedByLearners";
 import PaddingWrapper from "@/components/widgets/PaddingWrapper";
 
+export const metadata = {
+  title: "Help & Support Details Of Digital Academy 360 | Contact Us",
+  description:
+    "Toll Free Number To Get In Touch With Digital Academy 360 Dedicated Team For Enquiry, Admission, Help Or Support. You Are Just A Click Away. Contact Us Now!",
+  keywords: [
+    "Digital Academy 360 Help, Digital Academy 360 Support, Digital Academy 360 Toll Free, Contact Digital Academy 360",
+  ],
+  robots:
+    "index, follow, max-snippet:-1, max-video-preview:-1, max-image-preview:large",
+  alternates: {
+    canonical: "https://digitalacademy360.com/contact-us",
+  },
+  openGraph: {
+    title: "Help & Support Details Of Digital Academy 360 | Contact Us",
+    description:
+      "Toll Free Number To Get In Touch With Digital Academy 360 Dedicated Team For Enquiry, Admission, Help Or Support. You Are Just A Click Away. Contact Us Now!",
+    url: "https://digitalacademy360.com/contact-uss",
+    siteName: "Digital Academy 360",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Help & Support Details Of Digital Academy 360 | Contact Us",
+    description:
+      "Toll Free Number To Get In Touch With Digital Academy 360 Dedicated Team For Enquiry, Admission, Help Or Support. You Are Just A Click Away. Contact Us Now!",
+  },
+};
+
 export default function ContactUS() {
   return (
     <main>
