f74c20240c79b4eccd1fc87b2ce827962bb01890
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2025-07-10 18:33:22 +0530

    metatag update for placement

--- src/app/placements/page.tsx
+++ src/app/placements/page.tsx
@@ -9,6 +9,35 @@ import PlacementStats from "@components/placementStats";
 import StudentShowcase from "@components/studentShowcase";
 import PlacementsBanner from "@components/placementsBanner";
 
+export const metadata = {
+  title: "Digital Marketing Course Placements Report & Career Paths",
+  description:
+    "50K+ Learners Career Path Transformed Into Digital Marketers & Marketing Specialist. Explore Digital Marketing Career & Placement Record Of Digital Academy 360",
+  keywords: [
+    "Digital Marketing Career, Digital Marketing Specialists, Digital Marketing Career Path, Digital Marketing Placement",
+  ],
+  robots:
+    "index, follow, max-snippet:-1, max-video-preview:-1, max-image-preview:large",
+  alternates: {
+    canonical: "https://digitalacademy360.com/contact-us",
+  },
+  openGraph: {
+    title: "Digital Marketing Course Placements Report & Career Path",
+    description:
+      "50K+ Learners Career Path Transformed Into Digital Marketers & Marketing Specialist. Explore Digital Marketing Career & Placement Record Of Digital Academy 360",
+    url: "https://digitalacademy360.com/contact-us",
+    siteName: "Digital Academy 360",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Help & Support Details Of Digital Academy 360 | Contact Us",
+    description:
+      "Toll Free Number To Get In Touch With Digital Academy 360 Dedicated Team For Enquiry, Admission, Help Or Support. You Are Just A Click Away. Contact Us Now!",
+  },
+};
+
 export default function Career() {
   return (
     <main>
--- src/app/testimonial/page.tsx
+++ src/app/testimonial/page.tsx
@@ -2,6 +2,35 @@ import ReviewHighlights from "@components/reviewHighlights";
 import MarqueeTestimonials from "@components/marqueeTestimonials";
 import TestimonialsGrid from "@components/testimonialsGrid";
 
+export const metadata = {
+  title: "Digital Academy 360 | Reviews & Testimonials",
+  description:
+    "Reviews For Digital Academy 360's job-oriented courses gets you certified & placed. 50,000+ Careers Transformed, 1000+ Hiring Partner",
+  keywords: [
+    "digital academy 360 review, digital marketng course review, digital marketing institute review, job guaranteed course review",
+  ],
+  robots:
+    "index, follow, max-snippet:-1, max-video-preview:-1, max-image-preview:large",
+  alternates: {
+    canonical: "https://digitalacademy360.com/testimonial",
+  },
+  openGraph: {
+    title: "Digital Academy 360 | Reviews & Testimonials",
+    description:
+      "Reviews For Digital Academy 360's job-oriented courses gets you certified & placed. 50,000+ Careers Transformed, 1000+ Hiring Partner",
+    url: "https://digitalacademy360.com/testimonial",
+    siteName: "Digital Academy 360",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Digital Academy 360 | Reviews & Testimonials",
+    description:
+      "Reviews For Digital Academy 360's job-oriented courses gets you certified & placed. 50,000+ Careers Transformed, 1000+ Hiring Partner",
+  },
+};
+
 export default function Testimonial() {
   return (
     <main>
