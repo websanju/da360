cb45011cbe5ed89330737c972c77cc61b6ab7721
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2025-07-15 11:15:38 +0530

    code for home page

--- src/components/learnersSection/index.tsx
+++ src/components/learnersSection/index.tsx
@@ -9,8 +9,7 @@ import "aos/dist/aos.css";
 
 import UpArrow from "@/components/Ui/svg/arrowUp";
 import StudentArrow from "@/components/Ui/svg/studentArrow";
-import AccreditationSection from "@components/widgets/accreditationSection";
-import LearnSlider from "@components/learnSlider";
+// import LearnSlider from "@components/learnSlider";
 
 // Font configuration for Sora
 const sora = Sora({
@@ -140,10 +139,8 @@ export default function LearnersSection() {
   return (
     <section className={`${styles.learnersSection}`}>
       {/* Image Container */}
-      <div className={styles.accreditationSection}>
-        <AccreditationSection />
-      </div>
-      <LearnSlider />
+
+      {/* <LearnSlider /> */}
 
       <div className={styles.imageContainer}>
         <div className={styles.imageBox}>
