93c1ea71faad40f49f970e9e3d15cd26101538ec
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2025-07-16 08:40:05 +0530

    code for course page

--- src/components/aiTools/index.tsx
+++ src/components/aiTools/index.tsx
@@ -4,6 +4,24 @@ import styles from "./style.module.scss";
 import Image from "next/image";
 
 const toolCategories = {
+  All: [
+    { name: "Canva", logo: "/images/aitools/canva.png" },
+    { name: "Pixlr", logo: "/images/aitools/pixlr.png" },
+    { name: "Designify", logo: "/images/aitools/designify.png" },
+    { name: "Crello", logo: "/images/aitools/crello.png" },
+    { name: "Hatchful", logo: "/images/aitools/hatchful.png" },
+    { name: "Wix", logo: "/images/aitools/wix.png" },
+    { name: "Weebly", logo: "/images/aitools/weebly.png" },
+    { name: "Strikingly", logo: "/images/aitools/strikingly.png" },
+    { name: "Carrd", logo: "/images/aitools/carrd.png" },
+    { name: "Writesonic", logo: "/images/aitools/writesonic.png" },
+    { name: "QuillBot", logo: "/images/aitools/quillbot.png" },
+    { name: "Hemingway", logo: "/images/aitools/hemingway.png" },
+    { name: "Rytr", logo: "/images/aitools/rytr.png" },
+    { name: "Kapwing", logo: "/images/aitools/kapwing.png" },
+    { name: "VEED.IO", logo: "/images/aitools/veedio.png" },
+    { name: "Shortcut", logo: "/images/aitools/shortcut.png" },
+  ],
   Design: [
     { name: "Canva", logo: "/images/aitools/canva.png" },
     { name: "Pixlr", logo: "/images/aitools/pixlr.png" },
@@ -34,7 +52,7 @@ type CategoryName = keyof typeof toolCategories;
 
 const AiTools: React.FC = () => {
   const categoryNames = Object.keys(toolCategories) as CategoryName[];
-  const [activeTab, setActiveTab] = useState<CategoryName>("Design");
+  const [activeTab, setActiveTab] = useState<CategoryName>("All");
 
   return (
     <section className={styles.digitalToolsSection}>
