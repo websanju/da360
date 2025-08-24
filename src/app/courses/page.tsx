00644a733ae58b4b0d2f0b0d1724f8d56109e011
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2025-08-24 17:55:38 +0530

    In certification for course "Performance Marketing & MarTech" need to add the tags Add all section from the home page (after certification section) in the course page, PGCE In Performance Marketing & Media Buying | Tag - Classroom/ Online

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -6,6 +6,19 @@ import LeadershipSection from "@/components/courseList/leadershipSection";
 import PostGraduateSection from "@components/courseList/postGraduateSection";
 import CertificationSection from "@components/courseList/certificationSection";
 
+import OurSuccessStories from "@components/ourSuccessStories";
+// import styles from "./page.module.scss";
+import CommunityMeetupSlider from "@components/communityMeetupSlider";
+import GlobalCommunity from "@components/globalCommunity";
+import MarqueeBanner from "@components/widgets/marqueeBanner";
+import Comparison from "@components/comparison";
+// import LifeAtDA360 from "@components/lifeAtDA360";
+import LatestBlogPosts from "@components/latestBlogPosts";
+import MediaAwards from "@components/mediaAwards";
+import PaddingWrapper from "@components/widgets/PaddingWrapper";
+import GuestFacultySlider from "@components/guestFacultySlider";
+import BrandLogos from "@components/brandLogos";
+
 export default function About() {
   return (
     <main>
@@ -35,6 +48,28 @@ export default function About() {
               return null;
           }
         })}
+        <OurSuccessStories
+          headerTitle={"Our Alumni Achievements"}
+          description={"Real People, Real Transitions, Real Success"}
+          // breakpoints={{
+          //   480: { slidesPerView: 1.3 },
+          //   768: { slidesPerView: 2.3 },
+          //   1200: { slidesPerView: 4.3 },
+          // }}
+        />
+        <BrandLogos />
+        <PaddingWrapper desktopPaddingBottom="130px" mobilePaddingBottom="60px">
+          <GuestFacultySlider />
+        </PaddingWrapper>
+        <CommunityMeetupSlider />
+        <GlobalCommunity />
+        <MarqueeBanner />
+        <Comparison />
+        {/* <LifeAtDA360 /> */}
+        <MediaAwards />
+        <PaddingWrapper desktopPaddingBottom="130px" mobilePaddingBottom="60px">
+          <LatestBlogPosts />
+        </PaddingWrapper>
       </div>
     </main>
   );
