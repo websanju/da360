8b68b6c9428651d737e5991a94d8b1279083f0b8
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2025-08-24 21:48:58 +0530

    36 Partner Logos update

--- src/components/brandLogos/index.tsx
+++ src/components/brandLogos/index.tsx
@@ -3,25 +3,44 @@ import Image from "next/image";
 import styles from "./style.module.scss";
 import SectionHeader from "@components/widgets/sectionHeader";
 const logos = [
-  { src: "/images/brand/brand-1.png", alt: "Dentsu" },
-  { src: "/images/brand/brand-2.png", alt: "Valueleaf" },
-  { src: "/images/brand/brand-3.png", alt: "Jungle Square" },
-  { src: "/images/brand/brand-4.png", alt: "Google" },
-  { src: "/images/brand/brand-5.png", alt: "TCS " },
-  { src: "/images/brand/brand-6.png", alt: "KTM" },
-  { src: "/images/brand/brand-7.png", alt: "Zoomcar   " },
-  { src: "/images/brand/brand-8.png", alt: "Philips" },
-  { src: "/images/brand/brand-9.png", alt: "IBM" },
-  { src: "/images/brand/brand-10.png", alt: "Replicon" },
-  { src: "/images/brand/brand-11.png", alt: "AXA" },
-  { src: "/images/brand/brand-12.png", alt: "AXA" },
-  { src: "/images/brand/brand-13.png", alt: "Continental" },
-  { src: "/images/brand/brand-14.png", alt: "Simplilearn" },
-  { src: "/images/brand/brand-15.png", alt: "Bosch" },
-  { src: "/images/brand/brand-16.png", alt: "Oracle" },
-  { src: "/images/brand/brand-17.png", alt: "Unacademy" },
-  { src: "/images/brand/brand-18.png", alt: "AT&T" },
-  { src: "/images/brand/brand-19.png", alt: "Uplers" },
+  { src: "/images/brand/brand_1.jpg", alt: "Dentsu" },
+  { src: "/images/brand/brand_2.jpg", alt: "Valueleaf" },
+  { src: "/images/brand/brand_3.jpg", alt: "Jungle Square" },
+  { src: "/images/brand/brand_4.jpg", alt: "Google" },
+  { src: "/images/brand/brand_5.jpg", alt: "TCS " },
+  { src: "/images/brand/brand_6.jpg", alt: "KTM" },
+  { src: "/images/brand/brand_7.jpg", alt: "Zoomcar   " },
+  { src: "/images/brand/brand_8.jpg", alt: "Philips" },
+  { src: "/images/brand/brand_9.jpg", alt: "IBM" },
+  { src: "/images/brand/brand_10.jpg", alt: "Replicon" },
+  { src: "/images/brand/brand_11.jpg", alt: "AXA" },
+  { src: "/images/brand/brand_12.jpg", alt: "AXA" },
+  { src: "/images/brand/brand_13.jpg", alt: "Continental" },
+  { src: "/images/brand/brand_14.jpg", alt: "Simplilearn" },
+  { src: "/images/brand/brand_15.jpg", alt: "Bosch" },
+  { src: "/images/brand/brand_16.jpg", alt: "Oracle" },
+  { src: "/images/brand/brand_17.jpg", alt: "Unacademy" },
+  { src: "/images/brand/brand_18.jpg", alt: "AT&T" },
+];
+const logosSecond = [
+  { src: "/images/brand/brand_19.jpg", alt: "Dentsu" },
+  { src: "/images/brand/brand_20.jpg", alt: "Valueleaf" },
+  { src: "/images/brand/brand_21.jpg", alt: "Jungle Square" },
+  { src: "/images/brand/brand_22.jpg", alt: "Google" },
+  { src: "/images/brand/brand_23.jpg", alt: "TCS " },
+  { src: "/images/brand/brand_24.jpg", alt: "KTM" },
+  { src: "/images/brand/brand_25.jpg", alt: "Zoomcar   " },
+  { src: "/images/brand/brand_26.jpg", alt: "Philips" },
+  { src: "/images/brand/brand_27.jpg", alt: "IBM" },
+  { src: "/images/brand/brand_28.jpg", alt: "Replicon" },
+  { src: "/images/brand/brand_29.jpg", alt: "AXA" },
+  { src: "/images/brand/brand_30.jpg", alt: "AXA" },
+  { src: "/images/brand/brand_31.jpg", alt: "Continental" },
+  { src: "/images/brand/brand_32.jpg", alt: "Simplilearn" },
+  { src: "/images/brand/brand_33.jpg", alt: "Bosch" },
+  { src: "/images/brand/brand_34.jpg", alt: "Oracle" },
+  { src: "/images/brand/brand_35.jpg", alt: "Unacademy" },
+  { src: "/images/brand/brand_36.jpg", alt: "AT&T" },
 ];
 
 // Define the prop types
@@ -70,7 +89,7 @@ const BrandLogos: React.FC<BrandLogosProps> = ({
       {/* Row 2: Left to Right */}
       <div className={styles.slider}>
         <div className={`${styles.slideTrack} ${styles.leftToRight}`}>
-          {[...logos, ...logos].map((logo, index) => (
+          {[...logosSecond, ...logosSecond].map((logo, index) => (
             <div key={`ltr-${index}`} className={styles.slide}>
               <Image
                 src={logo.src}
