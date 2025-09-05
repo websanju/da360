d2449d4952e97d76e997a73c0a28f1dfdbbb8eae
sanjaykumbhar26 <webvedkumbhar@gmail.com>
2025-09-05 14:47:06 +0530

    Certification Courses added for all Courses pages

--- src/app/social-media-content-video-creator-course/certificationsData.ts
+++ src/app/social-media-content-video-creator-course/certificationsData.ts
@@ -1,14 +1,50 @@
 export const certificationsData = [
   {
-    name: "Inbound Marketing Certification by Hubspot",
-    image: "/images/da360.png",
+    name: "Content Marketing Certificate HubSpot Academy",
+    image: "/images/certificates/content_marketing_certificate_hubspot_academy.png",
   },
   {
-    name: "Google Ads Search Certification",
-    image: "/images/da360.png",
+    name: "Digital Academy 360 Course Completion Certificate",
+    image: "/images/certificates/digital_academy_360_course_completion_certificate.png",
   },
   {
-    name: "Course Completion Certificate by DA360",
-    image: "/images/da360.png",
+    name: "Google Ads Display Certification",
+    image: "/images/certificates/google_ads_display_certification.png",
+  },
+  {
+    name: "Google Analytics Certification",
+    image: "/images/certificates/google_analytics_certification.png",
+  },
+  {
+    name: "Google Search Ads Certification",
+    image: "/images/certificates/google_search_ads_certificate.png",
+  },
+  {
+    name: "Google Shopping Ads Certification",
+    image: "/images/certificates/google_shopping_ads_certificate.png",
+  },
+  {
+    name: "Digital Marketing Certificate HubSpot Academy",
+    image: "/images/certificates/hubspot_academy_digital_marketing_certificate.png",
+  },
+  {
+    name: "Email Marketing Certificate HubSpot Academy",
+    image: "/images/certificates/hubspot_academy_email_marketing_certificate.png",
+  },
+  {
+    name: "Inbound Marketing Certificate HubSpot Academy",
+    image: "/images/certificates/inbound_marketing_certificate_hubspot_academy.png",
+  },
+  {
+    name: "SEO Fundamentals Course Certificate Semrush Academy",
+    image: "/images/certificates/semrush_academy_seo_fundamentals_course_certificate.png",
+  },
+  {
+    name: "SEO Certified Certificate HubSpot Academy",
+    image: "/images/certificates/seo_certified_certificate_hubspot_academy.png",
+  },
+  {
+    name: "Social Media Certificate HubSpot Academy",
+    image: "/images/certificates/social_media_certificate_hubspot_academy.png",
   },
 ];
